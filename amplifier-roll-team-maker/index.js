b4bfc672964948054d01ac3fbe871ce053befdd7
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-01-17 10:40:33 +1100

    Make amplifier roll maker

---
+++ amplifier-roll-team-maker/index.js
@@ -0,0 +1,23 @@
+const textarea = document.getElementById("textarea")
+let teams = []
+function submit() {
+    const textareaValue = textarea.value
+    const textareaSplit = textareaValue.split("\n")
+    for (let i = 0; i < textareaSplit.length; i++) {
+        const textareaSplitValues = textareaSplit[i].split(",")
+        const team = {
+            "teamName": textareaSplitValues[0],
+            "player1Name": textareaSplitValues[1],
+            "player2Name": textareaSplitValues[2]
+        }
+        teams.push(team)
+    }
+
+    console.log(teams)
+    const jsonString = JSON.stringify(teams, null, 2);
+    const blob = new Blob([jsonString], { type: "application/json" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "teams.json";
+    link.click();
+}

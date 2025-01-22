d63900f1e36bd45e30cc16dd7e65e4e01e33d92b
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-01-22 23:29:46 +1100

    Continue updating amplifiers

--- api-test/index.js
+++ api-test/index.js
@@ -3,7 +3,7 @@ let player1Score = 897543
 let player1Combo = 876
 let player1Acc = 98.57
 let player1Misses = 2
-let player1Mods = "NF"
+let player1Mods = "EZNF"
 
 let player2ID = 8530809
 let player2Score = 764123
@@ -26,7 +26,7 @@ let player4Combo = 532
 let player4Acc = 98.65
 let player4Misses = 11
 let team2BaseScore = 1616496
-let player4Mods = "NF"
+let player4Mods = "EZNF"
 
 // Get address
 let address = ""
@@ -98,75 +98,81 @@ async function initialize() {
     runAPITest(17, amplifierID, "chili con carne", (player1Score + player2Score) * 2, player3Score + player4Score); await sleep(100)
     runAPITest(18, amplifierID, "qc kp", player1Score + player2Score, (player3Score + player4Score) * 2); await sleep(100)
 
-    // // Dude That Fingerlock
+    // Dude That Fingerlock
     amplifierID = 10
     runAPITest(19, amplifierID, "chili con carne", 906518+787047, 1616496); await sleep(100)
     runAPITest(20, amplifierID, "qc kp", 1661666, 973212 + 678665); await sleep(100)
 
-    // // Cold Clear Eyes I
+    // Cold Clear Eyes I
     amplifierID = 11
     runAPITest(21, amplifierID, "chili con carne", Math.round((player1Score + player2Score) * 1.05), player3Score + player4Score); await sleep(100)
     runAPITest(22, amplifierID, "qc kp", player1Score + player2Score, Math.round((player3Score + player4Score) * 1.05)); await sleep(100)
 
-    // // Cold Clear Eyes II
+    // Cold Clear Eyes II
     amplifierID = 12
     runAPITest(23, amplifierID, "chili con carne", Math.round((player1Score + player2Score) * 1.15), player3Score + player4Score); await sleep(100)
     runAPITest(24, amplifierID, "qc kp", player1Score + player2Score, Math.round((player3Score + player4Score) * 1.15)); await sleep(100)
 
-    // // Cold Clear Eyes III
+    // Cold Clear Eyes III
     amplifierID = 13
     runAPITest(amplifierID * 2 - 1, amplifierID, "chili con carne", Math.round((player1Score + player2Score) * 1.2), player3Score + player4Score); await sleep(100)
     runAPITest(amplifierID * 2, amplifierID, "qc kp", player1Score + player2Score, Math.round((player3Score + player4Score) * 1.2)); await sleep(100)
 
-    // // Gambler
-    // amplifierID = 14
-    // runAPITest(11, amplifierID, "chili con carne", 2077083, 1616496); await sleep(100)
-    // runAPITest(12, amplifierID, "qc kp", 1661666, 2020620); await sleep(100)
+    // Turn It Up
+    amplifierID = 14
+    runAPITest(amplifierID * 2 - 1, amplifierID, "chili con carne", Math.round(player1Score * 1.75) + player2Score, player3Score + Math.round(player4Score) * 1.75); await sleep(100)
+    runAPITest(amplifierID * 2, amplifierID, "qc kp", Math.round(player1Score * 1.75) + player2Score, player3Score + Math.round(player4Score) * 1.75); await sleep(100)
+    
+    // Gambler
+    amplifierID = 15
+    runAPITest(amplifierID * 2 - 1, amplifierID, "chili con carne", 2077083, 1616496); await sleep(100)
+    runAPITest(amplifierID * 2, amplifierID, "qc kp", 1661666, 2020620); await sleep(100)
 
     // // Make It Rock
-    // amplifierID = 18
-    // player4Score = 643284
-    // runAPITest(19, amplifierID, "chili con carne", 1661666, 2020620); await sleep(100)
-    // runAPITest(20, amplifierID, "qc kp", 2077083, 1616496); await sleep(100)
+    amplifierID = 18
+    player4Score = 643284
+    runAPITest(31, amplifierID, "chili con carne", 1661666, 2020620); await sleep(100)
+    runAPITest(32, amplifierID, "qc kp", 2077083, 1616496); await sleep(100)
 
     // // Yin And Yang I
-    // amplifierID = 20
-    // runAPITest(21, amplifierID, "chili con carne", 936294, 960074); await sleep(100)
-    // runAPITest(22, amplifierID, "qc kp", 891709, 1008077); await sleep(100)
+    amplifierID = 19
+    runAPITest(33, amplifierID, "chili con carne", 936294, 960074); await sleep(100)
+    runAPITest(34, amplifierID, "qc kp", 891709, 1008077); await sleep(100)
 
     // // Yin And Yang II
-    // amplifierID = 21
-    // runAPITest(23, amplifierID, "chili con carne", 980880, 960074); await sleep(100)
-    // runAPITest(24, amplifierID, "qc kp", 891709, 1056081); await sleep(100)
+    amplifierID = 20
+    runAPITest(35, amplifierID, "chili con carne", 980880, 960074); await sleep(100)
+    runAPITest(36, amplifierID, "qc kp", 891709, 1056081); await sleep(100)
 
     // // Yin And Yang III
-    // amplifierID = 22
-    // runAPITest(25, amplifierID, "chili con carne", 980880, 960074); await sleep(100)
-    // runAPITest(26, amplifierID, "qc kp", 891709, 1056081); await sleep(100)
-    // // Classic Farmer I
-    // amplifierID = 25
-    // runAPITest(27, amplifierID, "chili con carne", 1744749, 1616496); await sleep(100)
-    // runAPITest(28, amplifierID, "qc kp", 1661666, 1697321); await sleep(100)
+    amplifierID = 21
+    runAPITest(37, amplifierID, "chili con carne", 1025465, 960074); await sleep(100)
+    runAPITest(38, amplifierID, "qc kp", 891709, 1104085); await sleep(100)
 
-    // // Classic Farmer II
-    // amplifierID = 26
-    // runAPITest(29, amplifierID, "chili con carne", 1744749, 1616496); await sleep(100)
-    // runAPITest(30, amplifierID, "qc kp", 1661666, 1697321); await sleep(100)
+    // Classic Farmer I
+    amplifierID = 23
+    runAPITest(39, amplifierID, "chili con carne", 1744749, 1616496); await sleep(100)
+    runAPITest(40, amplifierID, "qc kp", 1661666, 1697321); await sleep(100)
 
-    // // Synchronised I
-    // amplifierID = 28
-    // runAPITest(31, amplifierID, "chili con carne", 1763028, 1616496); await sleep(100)
-    // runAPITest(32, amplifierID, "qc kp", 1661666, 1695704); await sleep(100)
+    // AccDance
+    amplifierID = 24
+    runAPITest(41, amplifierID, "chili con carne", 98.96, 99.16); await sleep(100)
+    runAPITest(42, amplifierID, "qc kp", 98.96, 99.16); await sleep(100)
 
-    // // Synchronised II
-    // amplifierID = 29
-    // runAPITest(33, amplifierID, "chili con carne", 1929194, 1616496); await sleep(100)
-    // runAPITest(34, amplifierID, "qc kp", 1661666, 1857354); await sleep(100)
+    // Synchronised I
+    amplifierID = 25
+    runAPITest(43, amplifierID, "chili con carne", 1763028, 1616496); await sleep(100)
+    runAPITest(44, amplifierID, "qc kp", 1661666, 1695704); await sleep(100)
+
+    // Synchronised II
+    amplifierID = 26
+    runAPITest(45, amplifierID, "chili con carne", 1929194, 1616496); await sleep(100)
+    runAPITest(46, amplifierID, "qc kp", 1661666, 1857354); await sleep(100)
 
     // // Go with the flow
-    // amplifierID = 30
-    // runAPITest(35, amplifierID, "chili con carne", 1910915, 1616496); await sleep(100)
-    // runAPITest(36, amplifierID, "qc kp", 1661666, 1858971); await sleep(100)
+    amplifierID = 27
+    runAPITest(47, amplifierID, "chili con carne", 1910915, 1616496); await sleep(100)
+    runAPITest(48, amplifierID, "qc kp", 1661666, 1858971); await sleep(100)
 
     // // Loadbearer I
     // amplifierID = 31
@@ -202,16 +208,6 @@ async function initialize() {
     // amplifierID = 39
     // runAPITest(49, amplifierID, "chili con carne", 1910916, 1616496); await sleep(100)
     // runAPITest(50, amplifierID, "qc kp", 1661666, 1858970); await sleep(100)
-
-    // // Snail
-    // amplifierID = 27
-    // runAPITest(51, amplifierID, "chili con carne", 98.96, 99.16); await sleep(100)
-    // runAPITest(52, amplifierID, "qc kp", 98.96, 99.16); await sleep(100)
-
-    // // Snail Sect
-    // amplifierID = 8
-    // runAPITest(53, amplifierID, "chili con carne", 98.96, 99.16); await sleep(100)
-    // runAPITest(54, amplifierID, "qc kp", 98.96, 99.16); await sleep(100)
 }
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))

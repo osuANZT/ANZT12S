445a447659c43f6ec38d76291b083a272a090809
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-01-29 21:09:35 +1100

    Complete amplifier team maker

--- amplifier-team-maker/index.js
+++ amplifier-team-maker/index.js
@@ -1,4 +1,23 @@
 const textareaEl = document.getElementById("textarea")
+let teams = []
 function submit() {
+    const textareaElValue = textareaEl.value
+    const textareaElValueIndividual = textareaElValue.split("\n")
+    for (let i = 0; i < textareaElValueIndividual.length; i++) {
+        const textareaElValueIndividualSeparated = textareaElValueIndividual[i].split(",")
+        const teamData = {
+            "player": textareaElValueIndividualSeparated[0],
+            "silver_amp": Number(textareaElValueIndividualSeparated[1]),
+            "gold_amp": Number(textareaElValueIndividualSeparated[2]),
+            "pris_amp": Number(textareaElValueIndividualSeparated[3])
+        }
+        teams.push(teamData)
+    }
 
+    const jsonString = JSON.stringify(teams, null, 2);
+    const blob = new Blob([jsonString], { type: "application/json" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "teams.json";
+    link.click();
 }
